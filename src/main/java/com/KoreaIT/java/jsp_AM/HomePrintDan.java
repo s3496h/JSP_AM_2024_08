c6a9a5b52671acce9d805b3d8f89ad5b0a6f62de
s3496h <s3496h@gmail.com>
2024-08-06 00:52:04 +0900

    서블릿과 jsp 활용

--- src/main/java/com/KoreaIT/java/jsp_AM/HomePrintDan.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/HomePrintDan.java
@@ -7,15 +7,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home/main2")
-public class HomeMainServlet2 extends HttpServlet {
 
-       
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+@WebServlet("/home/printDan")
+public class HomePrintDan extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		response.getWriter().append("Hello World!!!!!!!!!!!!!").append(request.getContextPath());
-	}
 
+
+		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
+	}
 }
