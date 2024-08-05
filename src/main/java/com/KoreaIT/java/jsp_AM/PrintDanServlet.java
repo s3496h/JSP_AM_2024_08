01307030386b2b6134ab4ef6a66c4c2477c298d5
s3496h <s3496h@gmail.com>
2024-08-05 23:08:24 +0900

    파라미터 사용

--- src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
@@ -7,27 +7,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/pringDan")
+@WebServlet("/printDan")
 public class PrintDanServlet extends HttpServlet {
-
-       
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		String inputedDan = request.getParameter("dan");
+		String inputedLimit = request.getParameter("limit");
+
 		
-		 response.setContentType("text/html;charset=UTF-8");
+		if (inputedDan == null) {
+			inputedDan = "1";
+		}
+
+		if (inputedLimit == null) {
+			inputedLimit = "1";
+		}
+
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+
+		response.getWriter().append(String.format("==%d단==<br>", dan));
+
 		
-		 response.getWriter().append("=8단= <br>");
-		 
-		 int dan = 8;
-		 
-		 for (int i = 1; i <= 9; i++) {
-				 
-		 
-		 response.getWriter().append(String.format("%d * %d = %d \n <br>" ,dan , i , dan * i));
+		for (int i = 1; i <= limit; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
+
 	}
-
-	
-
-}
 }
