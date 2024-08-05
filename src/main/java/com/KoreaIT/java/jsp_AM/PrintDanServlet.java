50a5aac3a0f80de797416e060f542d0c132d3d75
s3496h <s3496h@gmail.com>
2024-08-05 23:28:52 +0900

    파라미터 색상 추가

--- src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
@@ -1,39 +1,38 @@
 package com.KoreaIT.java.jsp_AM;
-
+import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 @WebServlet("/printDan")
 public class PrintDanServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		response.setContentType("text/html;charset=UTF-8");
 
 		String inputedDan = request.getParameter("dan");
 		String inputedLimit = request.getParameter("limit");
+		String inputedColor = request.getParameter("color");
 
-		
 		if (inputedDan == null) {
 			inputedDan = "1";
 		}
-
 		if (inputedLimit == null) {
 			inputedLimit = "1";
 		}
+		
 
 		int dan = Integer.parseInt(inputedDan);
 		int limit = Integer.parseInt(inputedLimit);
 
-		response.getWriter().append(String.format("==%d단==<br>", dan));
 
-		
+		response.getWriter().append(String.format("<div style='color:%s;'>==%d단==</div>", inputedColor, dan));
+
 		for (int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+
+			response.getWriter().append(
+					String.format("<div style=\"color:%s;\">%d * %d = %d</div>", inputedColor, dan, i, dan * i));
 		}
 
 	}
